2915e872f2112d0407fd5ec76d460d9b55508a9b
Trieste96 <truongminhtriet96@gmail.com>
2016-10-23 21:43:05 +0700

    Add ContactManager Solution

--- ConctactManager/Form1.cs
+++ ConctactManager/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ConctactManager
 {
-    public partial class Form1 : Form
+    public partial class fContactManager : Form
     {
-        public Form1()
+        public fContactManager()
         {
             InitializeComponent();
         }
