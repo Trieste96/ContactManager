0cfda2bb325c9eec77e5d413340bdb3b0b3a7ab3
Trieste96 <truongminhtriet96@gmail.com>
2016-10-25 22:11:06 +0700

    Rename solution and main project

--- ConctactManager/Form1.Designer.cs
+++ ConctactManager/Form1.Designer.cs
@@ -28,56 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabView = new System.Windows.Forms.TabPage();
-            this.tabAdd = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.tabControl1.SuspendLayout();
+            this.gridContacts = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabView.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridContacts)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabView);
-            this.tabControl1.Controls.Add(this.tabAdd);
-            this.tabControl1.Location = new System.Drawing.Point(1, 7);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(431, 295);
-            this.tabControl1.TabIndex = 0;
             // 
             // tabView
             // 
-            this.tabView.Controls.Add(this.dataGridView1);
+            this.tabView.Controls.Add(this.gridContacts);
             this.tabView.Location = new System.Drawing.Point(4, 22);
             this.tabView.Name = "tabView";
             this.tabView.Padding = new System.Windows.Forms.Padding(3);
             this.tabView.Size = new System.Drawing.Size(423, 269);
             this.tabView.TabIndex = 0;
-            this.tabView.Text = "Xem danh bạ";
+            this.tabView.Text = "Index";
             this.tabView.UseVisualStyleBackColor = true;
             // 
-            // tabAdd
+            // gridContacts
             // 
-            this.tabAdd.Location = new System.Drawing.Point(4, 22);
-            this.tabAdd.Name = "tabAdd";
-            this.tabAdd.Padding = new System.Windows.Forms.Padding(3);
-            this.tabAdd.Size = new System.Drawing.Size(423, 269);
-            this.tabAdd.TabIndex = 1;
-            this.tabAdd.Text = "Thêm";
-            this.tabAdd.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.gridContacts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(417, 266);
-            this.dataGridView1.TabIndex = 0;
+            this.gridContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridContacts.Location = new System.Drawing.Point(3, 0);
+            this.gridContacts.Name = "gridContacts";
+            this.gridContacts.Size = new System.Drawing.Size(420, 266);
+            this.gridContacts.TabIndex = 0;
+            this.gridContacts.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.gridContacts_CellMouseUp);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabView);
+            this.tabControl1.Location = new System.Drawing.Point(1, 7);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(431, 295);
+            this.tabControl1.TabIndex = 0;
             // 
             // fContactManager
             // 
@@ -88,19 +80,18 @@
             this.Name = "fContactManager";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý danh bạ";
-            this.tabControl1.ResumeLayout(false);
             this.tabView.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridContacts)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabView;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TabPage tabAdd;
+        private System.Windows.Forms.DataGridView gridContacts;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
--- ConctactManager/Form1.cs
+++ ConctactManager/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConctactManager.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace ConctactManager
 {
     public partial class fContactManager : Form
     {
+        private ContactManagerDBEntities _entities = new ContactManagerDBEntities();
+        private void initControls()
+        {
+            gridContacts.ReadOnly = true;
+            gridContacts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void indexView()
+        {
+            gridContacts.DataSource = _entities.Contacts.ToList();
+        }
         public fContactManager()
         {
             InitializeComponent();
+            initControls();
+            indexView();
+        }
+
+        private void Edit_view()
+        {
+
+        }
+        private void gridContacts_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                ContextMenuStrip menu = new ContextMenuStrip();
+                menu.Items.Add("Edit", null);
+                menu.Items.Add("Delete", null);
+
+                DataGridView grid = sender as DataGridView;
+                grid.CurrentCell = grid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                Point pt = grid.PointToClient(Control.MousePosition);
+                System.Console.WriteLine("Mouse's location: "+Control.MousePosition.X + ", " + Control.MousePosition.Y);
+                System.Console.WriteLine("Point's grid location: "+pt.X + ", " + pt.Y);
+                menu.Show(grid, pt);
+            }
+            catch (ArgumentOutOfRangeException)
+            {   }
         }
     }
 }
