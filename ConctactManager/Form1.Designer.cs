dc4d24cf394b3ed91c6104a6ef5ff7a9a669a2ef
Trieste96 <truongminhtriet96@gmail.com>
2016-10-29 02:52:03 +0700

    Save before window 10 reinstallation

--- ConctactManager/Form1.Designer.cs
+++ ConctactManager/Form1.Designer.cs
@@ -32,15 +32,15 @@
             this.gridContacts = new System.Windows.Forms.DataGridView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtFname = new System.Windows.Forms.TextBox();
-            this.txtLname = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtPhone = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabView.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridContacts)).BeginInit();
             this.tabControl1.SuspendLayout();
@@ -66,7 +66,7 @@
             this.gridContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridContacts.Location = new System.Drawing.Point(3, 0);
             this.gridContacts.Name = "gridContacts";
-            this.gridContacts.Size = new System.Drawing.Size(417, 266);
+            this.gridContacts.Size = new System.Drawing.Size(377, 266);
             this.gridContacts.TabIndex = 0;
             this.gridContacts.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.gridContacts_CellMouseClick);
             // 
@@ -102,53 +102,16 @@
             this.tabPage1.Text = "Add";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // btnSave
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "First name";
-            // 
-            // txtFname
-            // 
-            this.txtFname.Location = new System.Drawing.Point(153, 51);
-            this.txtFname.Name = "txtFname";
-            this.txtFname.Size = new System.Drawing.Size(196, 20);
-            this.txtFname.TabIndex = 1;
-            // 
-            // txtLname
-            // 
-            this.txtLname.Location = new System.Drawing.Point(153, 89);
-            this.txtLname.Name = "txtLname";
-            this.txtLname.Size = new System.Drawing.Size(196, 20);
-            this.txtLname.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Last name";
-            // 
-            // txtPhone
-            // 
-            this.txtPhone.Location = new System.Drawing.Point(153, 125);
-            this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(196, 20);
-            this.txtPhone.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 128);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Phone";
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(201, 197);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 40);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // txtEmail
             // 
@@ -166,15 +129,53 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "E-mail";
             // 
-            // btnSave
+            // txtPhone
             // 
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(201, 197);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 40);
-            this.btnSave.TabIndex = 8;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.txtPhone.Location = new System.Drawing.Point(153, 125);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(196, 20);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(153, 89);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(196, 20);
+            this.txtLname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last name";
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(153, 51);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(196, 20);
+            this.txtFname.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name";
             // 
             // fContactManager
             // 
--- ConctactManager/Form1.cs
+++ ConctactManager/Form1.cs
@@ -17,10 +17,7 @@ namespace ConctactManager
         private void indexView()
         {
             gridContacts.DataSource = _entities.Contacts.ToList();
-        }
-        private void editView()
-        {
-            
+            tabControl1.SelectedTab = tabView;
         }
         public fContactManager()
         {
@@ -28,7 +25,15 @@ namespace ConctactManager
             initControls();
             indexView();
         }
-
+        private void Add_view()
+        {
+            tabPage1.Text = "Add";
+            btnSave.Tag = null;
+            txtFname.Clear();
+            txtLname.Clear();
+            txtPhone.Clear();
+            txtEmail.Clear();
+        }
         private void Edit_view(Contact contactToEdit)
         {
             tabPage1.Text = "Edit";
@@ -37,8 +42,9 @@ namespace ConctactManager
             txtPhone.Text = contactToEdit.Phone;
             txtEmail.Text = contactToEdit.Email;
             btnSave.Tag = contactToEdit.Id;
+            tabControl1.SelectedTab = tabPage1;
         }
-        private void Edit_click()
+        private void Edit_click(object sender, EventArgs e)
         {
             int id = (int)gridContacts.CurrentRow.Cells[0].Value;
             var contactToEdit = ( 
@@ -48,7 +54,25 @@ namespace ConctactManager
                 ).FirstOrDefault();
             Edit_view(contactToEdit);
         }
+        private void delete_click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xoá học sinh này khỏi CSDL?", "Xoá", MessageBoxButtons.OKCancel);
+            if(result == DialogResult.OK)
+            {
+                Contact contactToDelete = new Contact() {
+                    Id = (int)gridContacts.SelectedRows[0].Cells[0].Value,
+                    FirstName = gridContacts.SelectedRows[0].Cells[1].Value.ToString(),
+                    LastName = gridContacts.SelectedRows[0].Cells[2].Value.ToString(),
+                    Phone = gridContacts.SelectedRows[0].Cells[3].Value.ToString(),
+                    Email = gridContacts.SelectedRows[0].Cells[4].Value.ToString(),
+                };
+                _entities.Entry(contactToDelete).State = System.Data.Entity.EntityState.Added;
+                _entities.Contacts.Remove(contactToDelete);
 
+                _entities.SaveChanges();
+                indexView();
+            }
+        }
         private void gridContacts_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             MouseEventArgs me = (MouseEventArgs)e;
@@ -56,7 +80,7 @@ namespace ConctactManager
             {
                 ContextMenuStrip menu = new ContextMenuStrip();
                 menu.Items.Add("Edit", null, new EventHandler(Edit_click));
-                menu.Items.Add("Delete", null);
+                menu.Items.Add("Delete", null, new EventHandler(delete_click));
 
                 DataGridView grid = sender as DataGridView;
                 grid.CurrentCell = grid.Rows[e.RowIndex].Cells[e.ColumnIndex];
@@ -64,6 +88,35 @@ namespace ConctactManager
                 //System.Console.WriteLine("Mouse's location: " + Control.MousePosition.X + ", " + Control.MousePosition.Y);
                 //System.Console.WriteLine("Point's grid location: " + pt.X + ", " + pt.Y);
                 menu.Show(grid, pt);
+            }
+        }
+        private Contact getContactToCreate()
+        {
+            return new Contact(){
+                Id = btnSave.Tag == null ? 0 : (int)(btnSave.Tag),
+                FirstName = txtFname.Text.Trim(),
+                LastName = txtLname.Text.Trim(),
+                Phone = txtPhone.Text.Trim(),
+                Email = txtEmail.Text.Trim()
+            };
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Contact contactToEdit = getContactToCreate();
+            Console.WriteLine(contactToEdit.Id);
+            if(contactToEdit.Id > 0)
+            {
+                var originalContact = (
+                    from c in _entities.Contacts
+                    where c.Id == contactToEdit.Id
+                    select c
+                    ).FirstOrDefault();
+                _entities.Database.BeginTransaction();
+                _entities.Entry(originalContact).CurrentValues.SetValues(contactToEdit);
+                _entities.SaveChanges();
+                MessageBox.Show("Edit successfully!");
+                indexView();
+                Add_view();
             }
         }
     }
